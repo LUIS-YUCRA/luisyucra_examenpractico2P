324f661c1248dae117ef21a8cd2238d225cd8a61
luisiño <luchitoyucra664@gmail.com>
2025-04-23 11:57:45 -0400

    LUIS YUCRA

--- septimo.js
+++ septimo.js
@@ -3,6 +3,7 @@ function suma(x) {
     return x+1;
 }
 
+const suma = (x) => x+1;
 // 2. Dado el siguiente arreglo. Mostrar solo los valores del objeto paciente usando for..in
 
 let paciente = {
@@ -11,6 +12,9 @@ let paciente = {
     genero: "Masculino",
     diagnostico: "Hipertensión",
     tratamiento: "Medicación diaria"
+}
+for (let mostrar in paciente) {
+    console.log(mostrar, paciente[mostrar]);
 }
 
 // 3. Dado el arreglo de objetos añadir un objeto carro al inicio del arreglo y desplegar 
@@ -30,10 +34,53 @@ let carros = [
         modelo: 2020,
         marca: "toyota"
     },
- ]
-
+]
+carro = carros.unshift({
+    color: "azul",
+    modelo: 2020,
+    marca: "suzuki"
+});
+ console.log(carros);
  // 4. Con el nuevo arreglo que obtuviste en el ejercicio 3  crear una funcion que me devuelva 
  // y muestre en consola el numero de carros modelo 2020
 
- // 5. Dado una arreglo de nombres ["argentia","bolivia","colombia"] realizar un funcion callbak para
+ function modelo(arreglo){
+ let contador = 0;
+ for (let carros of arreglo){
+    if (carros.modelo === 2020){
+        contador++;
+    } 
+}
+console.log("numero de modelos de carros: " + contador);
+return contador;
+ }
+ modelo(carros);
+ // 5. Dado una arreglo de nombres ["argentina","bolivia","colombia"] realizar un funcion callbak para
  // pasar el arreglo de minisculas a mayusculas.
+
+ let paisA = "bolivia" 
+ {
+  let  dept1 = "la paz";
+  let  dept2 = "oruro";
+ }
+ console.log(paisA)
+
+ let paisB = "Argentina"
+{
+    let depto1 = "Buenos Aires";
+    let depto2 = "Rosario";
+   
+}
+console.log(paisB);
+
+let paisC = "colombia"
+{
+    let ciudad1 = "guayaquil";
+    let ciudad2 = "sinaloa";
+}
+console.log(paisC);
+
+function mayuscula(nombre){
+    return nombre.toUpperCase();
+}
+
