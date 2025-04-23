b2007c1ba97ab93c4e01395df35824a172a15010
milenacarola <milenacarola19@gmail.com>
2025-04-23 07:45:23 -0400

    examen septimo

---
+++ septimo.js
@@ -0,0 +1,39 @@
+// 1. hallar la funcion flecha
+function suma(x) {
+    return x+1;
+}
+
+// 2. Dado el siguiente arreglo. Mostrar solo los valores del objeto paciente usando for..in
+
+let paciente = {
+    nombre: "Carlos",
+    edad: 35,
+    genero: "Masculino",
+    diagnostico: "Hipertensión",
+    tratamiento: "Medicación diaria"
+}
+
+// 3. Dado el arreglo de objetos añadir un objeto carro al inicio del arreglo y desplegar 
+let carros = [
+    {
+        color: "morado",
+        modelo: 2020,
+        marca: "toyota"
+    },
+    {
+        color: "negro",
+        modelo: 2010,
+        marca: "toyota"
+    },
+    {
+        color: "blanco",
+        modelo: 2020,
+        marca: "toyota"
+    },
+ ]
+
+ // 4. Con el nuevo arreglo que obtuviste en el ejercicio 3  crear una funcion que me devuelva 
+ // y muestre en consola el numero de carros modelo 2020
+
+ // 5. Dado una arreglo de nombres ["argentia","bolivia","colombia"] realizar un funcion callbak para
+ // pasar el arreglo de minisculas a mayusculas.
